fedf695afd8eb93ad13381acb8bbbf4ba5d3e348
Ariel Juodziukynas <arieljuod@gmail.com>
2019-04-10 16:45:52 -0300

    Scope functions to prevent possible name conflicts

--- app/assets/javascripts/vanilla_nested.js
+++ app/assets/javascripts/vanilla_nested.js
@@ -1,27 +1,28 @@
-document.addEventListener('click', function(e) {
-  if (e.target.classList.contains('vanilla-nested-add')) {
-    e.preventDefault();
-    addVanillaNestedFields(e.target);
+(function(){
+  document.addEventListener('click', function(e) {
+    if (e.target.classList.contains('vanilla-nested-add')) {
+      e.preventDefault();
+      addVanillaNestedFields(e.target);
+    }
+
+    if (e.target.classList.contains('vanilla-nested-remove')) {
+      e.preventDefault();
+      removeVanillaNestedFields(e.target);
+    }
+  })
+
+  function addVanillaNestedFields(btn) {
+    const auxDiv = document.createElement('div');
+    auxDiv.innerHTML = btn.dataset.html.replace(/_idx_placeholder_/g, Date.now());
+    const toInsert = auxDiv.children[0];
+    const container = document.querySelector(btn.dataset.containerSelector);
+    if (btn.dataset.methodForInsert == 'append') container.appendChild(toInsert);
+    else if (btn.dataset.methodForInsert == 'prepend') container.insertBefore(toInsert, container.children[0]);
   }
 
-  if (e.target.classList.contains('vanilla-nested-remove')) {
-    e.preventDefault();
-    removeVanillaNestedFields(e.target);
+  function removeVanillaNestedFields(btn) {
+    const wrapper = btn.parentElement;
+    wrapper.style.display = 'none';
+    wrapper.querySelector('[name$="[_destroy]"]').value = '1';
   }
-})
-
-addVanillaNestedFields = function(btn) {
-  let html = btn.dataset.html.replace(/_idx_placeholder_/g, Date.now());
-  let auxDiv = document.createElement('div');
-  auxDiv.innerHTML = html;
-  let toInsert = auxDiv.children[0];
-  const container = document.querySelector(btn.dataset.containerSelector);
-  if (btn.dataset.methodForInsert == 'append') container.appendChild(toInsert);
-  else if (btn.dataset.methodForInsert == 'prepend') container.insertBefore(toInsert, container.children[0]);
-}
-
-removeVanillaNestedFields = function(btn) {
-  const wrapper = btn.parentElement;
-  wrapper.style.display = 'none';
-  wrapper.querySelector('[name$="[_destroy]"]').value = '1';
-}
+})()
